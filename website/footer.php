c37709140dab8600d94a95448fa92780001715a5
Jay Sapoznikov <skyfor3@gmail.com>
2015-04-17 03:53:33 +0100

    news & footer

---
+++ website/footer.php
@@ -0,0 +1,47 @@
+
+<footer>
+<br>
+<div class="footer-distributed">
+
+			<div class="footer-left">
+
+				<p class="footer-links">
+					<a href="#">Home</a>
+					·
+					<a href="#">Blog</a>
+					·
+					<a href="#">Pricing</a>
+					·
+					<a href="#">About</a>
+					·
+					<a href="#">Faq</a>
+					·
+					<a href="#">Contact</a>
+				</p>
+
+				<p class="footer-company-name">Indie Games Sheffield © 2015</p>
+
+			</div>
+
+			<div class="footer-right" style="display:none;">
+
+				<form method="get" action="#">
+					<input placeholder="Search our website" name="search">
+					<i class="fa fa-search"></i>
+				</form>
+
+			</div>
+
+		</div>
+
+</footer>
+<style>
+footer {
+  background-color: #eee;
+  position: relative;
+  left: 0;
+  bottom: 0;
+  height: 100px;
+  width: 100%;
+  }
+  </style>
